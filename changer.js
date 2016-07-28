dc8cdbfd3860f2e49db64411ac4d8c52de43322d
Zach Liou <zach@trackrevenue.com>
2016-07-28 13:02:06 -0700

    Implement changer v1

--- changer.js
+++ changer.js
@@ -7,6 +7,11 @@
 (function() {
     "use strict";
 
+    var DESTINATION = "https://www.google.com";
 
+    var elems = document.querySelectorAll("a");
+    for (var i = 0; i < elems.length; ++i) {
+        elems[i].setAttribute("href", DESTINATION);
+    }
 
 })();
